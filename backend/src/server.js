e02d8ba89bf4ba7e8619b64d8e31be1864d1fe7d
Abhinav Tripathi <138208075+AbhinavTripathi63@users.noreply.github.com>
2026-03-13 16:46:45 +0530

    Fix server.js deployment issues

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,14 +23,14 @@ app.use(express.json({ limit: "10kb" }));
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN,
+    origin: process.env.CLIENT_ORIGIN || "*",
     credentials: true
   })
 );
 
 app.use(morgan("dev"));
 
-// -------------------- Rate Limiter (Auth only) --------------------
+// -------------------- Rate Limiter --------------------
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 30,
